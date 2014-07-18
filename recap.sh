4fafb74f2bd4b439836c6abd9649f5684bcfd628
Jean-Philippe CARUANA <jp@target2sell.com>
2014-07-18 14:02:50 +0200

    shell script: input control (nb args)

--- recap.sh
+++ recap.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+EXPECTED_ARGS=5
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` <start-time> <end-time> <file-to-parse> <one-word-email-title> <email-to>"
+  echo "       with start-time and end-time in hh:mm[:ss] format"
+  echo
+  echo "       example: `basename $0` 11:00 11:59:59 /var/log/front.log front john@acme.com"
+  exit 1
+fi
 
 START_TIME=$1
 END_TIME=$2
 FILE_NAME=$3
 TITLE=$4
 DEST=$5
-
-# TODO control input
 
 all_file=/tmp/${TITLE}.log
 python log.py ${START_TIME} ${END_TIME} ${FILE_NAME} > ${all_file}
